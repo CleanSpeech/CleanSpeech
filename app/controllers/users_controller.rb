f51dd8cfffebdc331c38c5d6a85d04d124d2ad3d
Jessica Lombera <jesstlombera@gmail.com>
2015-05-09 14:27:18 -0700

    Added partials and id of mic-start on the index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,24 @@
 class UsersController < ApplicationController
-	def index
+	# Please note that navigation is on 
+	#the layouts/application file
 
+	#
+
+	def index
 		render :index
 	end
 
 	def show
 		render :login
+		
 	end
 
 	def new
 		render :signup
+
+	end
+
+	def speech
+		render :speeches
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: "users#index"
+ 
+  # REST routes
 
   get '/users', to: 'users#index'
   get '/users/signup', to: 'users#new'
+  get '/users/login', to: 'users#show'
+  get '/users/speeches', to: 'users#speech'
+
 end
