8dd68d49e7c048e5bca6a0b115bb64d71328edc3
Jessica Lombera <jesstlombera@gmail.com>
2015-05-12 15:29:07 -0700

    style the index hover over mic

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 	root to: 'home#index'
 	  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
 	  resources :users
+	  resources :home
 end
