e046c80aa7c7ac8b9b96ca0b45e7d00c289265b3
Allxie <allxiecleary@gmail.com>
2015-05-12 14:53:23 -0700

    got swears counter and words counter to workgit statusgit statusgit status

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,6 @@ $(function () {
 	recognition.continuous = true;
 	//all the words are going to go 
 	//into this array individually
-	var transArray = [""];
 	var stopClicked = false;
 	//grabs our button that starts listening
 	$("#mic-start-button").click(function (){
@@ -56,92 +55,53 @@ $(function () {
 			// times the user used them
 			var showResults = function(){
 				str = "";
+				if (fillers.indexOf("*swears") == -1){
+					fillers.push("*swears");
+				};
 				for (var x = 0; x < fillers.length; x++){
 					console.log("Fillers[x]: " + fillers[x]);
 					str += (fillers[x] + ": " + fillerWordCounts[x] + "<br>");
 				}
-				//console.log("str : " + str);
-
+				console.log("fillers in show results: ", fillers);
 				console.log("stopClicked: " + stopClicked);
-				// RESET THING THAT DOESN'T WORK AGAIN
-				// if (!stopClicked){
-				// 		console.log("stopping");
-				// 		recognition.stop().then(function (thing){
-				// 			recognition.start();
-				// 		});
-				// 		console.log("stopped");
-						
-				// 		console.log("starting again");
-				// };
 				return str;
-			}
+			};
 
 			//puts the transcript on the page
 			$("#textHere").html(transcript);
 			var fillerWordCounts = pickFillers(wordArray, fillers);
 			$("#count").html(showResults());
-			//console.log("Inside: " + transArray);
-
 		}
 		recognition.start();
 		console.log("starting!!")
 		//calls listener function
-		
 
+		//console.log(fillers);
+		
 	}); //end onclick start
 
-	// RESET THING THAT DOESN'T WORK
-	// recognition.onaudioend = function (event){
-	// 	console.log("stopClicked: " + stopClicked);
-	// 	if (!stopClicked){
-	// 			recognition.start();
-	// 			console.log("starting again");
-	// 	}
-	// };
+var fillers = [];
+recognition.onresult = function(event){
+	console.log("OUTSIDE ONRESULT!");
 
-		//stops the recognition 
-	$("#stopButton").click(function(){
-		stopClicked = true;
-		console.log("stopClicked in stop button: " + stopClicked);
-		recognition.stop();
-	});
-
-	//grabs the form where users enter words
-	var $addWordForm = $("#fillerWordInput");
-	//when user clicks submit...
-	$addWordForm.on("submit", function (event) {
-		//so that the page doesn't reload onclick
-		event.preventDefault();
-		var word = $("#newFiller").val();
-		fillers.push(word);
-		$.post("/words.json", {
-			word: {
-				word: word
-			}
-		}).done(function (createdWord){
-			console.log(fillers);
-		$("#displayFillers").append(createdWord.word + "<br>");
-		})
-	});
-		
-
-// this function is going to find our filler 
-// words in the array of total words
-// and count them	
-	var fillers = [];
-
+}
 	var pickFillers = function(transcript, fillers){
 		//console.log("transcript: " + transcript);
 		// initializes an array that's the 
 		// length of the filler words array
-		var counts = new Array(fillers.length);
-		//console.log("counts.length : " + counts.length);
-		//console.log ("Transcript.length : " + transcript.length);
-
+		var counts = counts || fillers.slice();
+		counts.forEach(function(el,id,arr){
+			arr[id] = 0;
+		});
+		if (fillers.indexOf("*swears") == -1 && counts.length < fillers.length + 1 ){
+		counts.push(0);
+		}
+		// this is to add one at the end to count swears
+		
 		//cycles through filler words
 		for (var k = 0; k < fillers.length; k++ ){
 			var filler = fillers[k];
-			counts[k] = 0;
+			counts[k] = counts[k] || 0;
 			//if there's a space in the filler word...
 			//which means that there are two words in the filler
 			if (filler.indexOf(" ") != -1){
@@ -157,17 +117,43 @@ $(function () {
 				}
 			} else {
 				//cycles through whole transcript
-				for (var i = 0; i < transcript.length; i++){
-					if (transcript[i] == filler){
-						console.log(transcript[i]);
+				for (var q = 0; q < transcript.length; q++){
+					if (transcript[q] == filler){
 						counts[k] += 1;
+					} else if (transcript[q][1] == "*"){ //THIS NEEDS TO BE MADE CONDITIONAL
+						counts[counts.length-1] += 1;
 					}
 				} 
 			}
 		}
-		//console.log("Counts: ", counts);
+		console.log("fillers:", fillers);
+		console.log("Counts: ", counts);
+		counts[counts.length-1] = counts[counts.length-1]/2;
 		return counts;
 	};
+
+	//grabs the form where users enter words
+	var $addWordForm = $("#fillerWordInput");
+	//when user clicks submit...
+	$addWordForm.on("submit", function (event) {
+		//so that the page doesn't reload onclick
+		event.preventDefault();
+		var word = $("#newFiller").val();
+		fillers.push(word);
+		$.post("/words.json", {
+			word: {
+				word: word
+			}
+		}).done(function (createdWord){
+			console.log(fillers);
+			$("#displayFillers").append(createdWord.word + "<br>");
+		})
+	});
+		
+// this function is going to find our filler 
+// words in the array of total words
+// and count them	
+	
 	
 	function timer(){
 		var s = -1;
@@ -203,16 +189,21 @@ $(function () {
 		  }
 		};
 
-		$("#stopButton").click(function(){
-			clearInterval(newTimer);
+	$("#stopButton").click(function(){
+		stopClicked = true;
+		console.log("stopClicked in stop button: " + stopClicked);
+		recognition.stop();
+		clearInterval(newTimer);
 		});
-	}; // end
+	}; // end timer
 
 
 // To Do:
 
-// -- Figure out data structures for collecting all words
+// -- Fix data structures for collecting all words
 //    vs filler words
+// -- create default user for people not logged in on which
+//		to attach collected words
 // -- FOR ANYONE READING MY COMMENTS, I AM SO SORRY. 
 // 		IT'S SO THAT PEOPLE STOP SAYING THESE WORDS.
 // -- need to fix : fuck, fucking, fucker, motherfucker, whore, shit, 
@@ -220,13 +211,11 @@ $(function () {
 // 		bullshit, bumblefuck, cockhead, cocksucker, cum, 
 // 		cumslut, slut s***, dickface, dicksucker, dipshit, dumbfuck (but not dumbshit??)
 // 		fag, fagbag, fagfucker, faggot, motherfucking, nigger, nigga, niggers,
-// -- add bigram iteration to find "you know" etc.
 
-// if stopbutton has been clicked on, then end. If not, 
-// restart when onend has been triggered. 
 
-//create default user for people not logged in on which
-//to attach collected words
+
+//
+
 });
 
 
