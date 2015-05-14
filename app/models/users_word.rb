f837357f43bbc1976630dc01e78d40e139be3c05
Allxie <allxiecleary@gmail.com>
2015-05-13 17:42:09 -0700

    omg fixed word_speech_attempts table

--- app/models/users_word.rb
+++ app/models/users_word.rb
@@ -3,6 +3,4 @@ class UsersWord < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :word
 
-	has_many :speech_attempts, :through => :users_words_speech_attempts
-	has_many :users_words_speech_attempts
 end
