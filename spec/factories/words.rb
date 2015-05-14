41848a5fc26789fc0b7326c8b8005229a72d4456
Michael Reekers <mreekers@Michaels-MacBook-Air.local>
2015-05-13 20:19:25 -0700

    added two additional factories for speechattempts and words

--- spec/factories/words.rb
+++ spec/factories/words.rb
@@ -6,15 +6,9 @@ FactoryGirl.define do
     
     # # Define a basic devise user.
     factory :word do |f| 
-        #string is the proper call to produce just
-        #a string for the word 
+        #HipsterIpsum is the proper call to produce a
+        #random word 
         f.word { FFaker::HipsterIpsum.word }
     end
 end
 
-# FactoryGirl.define do
-#   factory :blah do
-#     name "dummy"
-#     alias "dummy"
-#   end
-# end
